258429011b2e44f95ab764ce1eb7224d80fb6df2
srz <severin.raez@bluewin.ch>
2016-07-26 02:29:38 +0200

    Refactor.

--- goedel.rb
+++ goedel.rb
@@ -1,25 +1,33 @@
 #!/bin/ruby
 
+# DEPENDENCIES
 require 'neo4j'
 require 'pp'
 require 'sinatra'
 
-require_relative 'packages/graph/bootstrap'
-require_relative 'packages/graph/city'
-require_relative 'packages/fdsl/fdsl'
+def require_package(name)
+  require_relative 'packages/' + name
+end
 
+require_package 'graph/bootstrap'
+require_package 'graph/city'
+require_package 'fdsl/fdsl'
+
+# CONFIGURATION
 dir_path = File.dirname(__FILE__)
 config_path = dir_path + '/config.yml'
 config = YAML.load(File.read(config_path))
 
 Graph::Bootstrap.new(config['db']).run
 
+# NOTES
 # create (c:City {name:'Bern', latitude: 46.9479, longitude: 7.4446})
 
 #c = Graph::City.create(name: 'Teststadt', latitude: 46.9479, longitude: 7.4446)
 
 # match (c:City) with c call spatial.addNode('test_layer', c) yield node return count(*)
 
+# MISSION
 f = FDSL.new
 fi = f # Impure functions
 
